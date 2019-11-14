ccac9eaaf531912be9df018cca8380847f2bf989
Will Reiske <wreiske@mieweb.com>
2019-11-14 14:57:04 -0500

    some fixes

--- package.js
+++ package.js
@@ -3,120 +3,120 @@ const fs = Npm.require('fs');
 const wreiskeIonic4PackageName = 'wreiske-ionic4';
 
 Package.describe({
-	name: 'wreiske:ionic4',
-	version: '4.8.1', // Meteor package wrap number https://docs.meteor.com/api/packagejs.html#PackageNamespace-describe
-	summary: 'Automatically import Ionic4 Web Components into your Client templates',
-	git: 'https://github.com/wreiske/meteor-ionic4.git',
-	documentation: 'README.md'
+  name: 'wreiske:ionic4',
+  version: '4.8.1', // Meteor package wrap number https://docs.meteor.com/api/packagejs.html#PackageNamespace-describe
+  summary: 'Automatically import Ionic4 Web Components into your Client templates',
+  git: 'https://github.com/wreiske/meteor-ionic4.git',
+  documentation: 'README.md'
 });
 
 Package.onUse(api => {
-	// We can now bundle the @ionic/core loader with the rest of the Meteor App code.
-	// It will read the above hard-coded path instead of guessing it.
-	api.addFiles(path.join('dist', 'ionic.js'), 'client');
-	// Add the Ionic CSS file (introduced in version 4.0.0-alpha.8).
-	// Renamed from "ionic.css" to "ionic.bundle.css" in version 4.0.0-beta.6.
-	api.addFiles(path.join('css', 'ionic.bundle.css'), 'client');
+  // We can now bundle the @ionic/core loader with the rest of the Meteor App code.
+  // It will read the above hard-coded path instead of guessing it.
+  api.addFiles(path.join('dist', 'ionic.js'), 'client');
+  // Add the Ionic CSS file (introduced in version 4.0.0-alpha.8).
+  // Renamed from "ionic.css" to "ionic.bundle.css" in version 4.0.0-beta.6.
+  api.addFiles(path.join('css', 'ionic.bundle.css'), 'client');
 
-	// Within package.js we do not have access to `process` or `__dirname`, so we have to assume the path…
-	const wreiskeIonic4PackagePath = isMeteorProject('.') ?
-		getWReiskeIonic4Package('.') :
-		'.';
+  // Within package.js we do not have access to `process` or `__dirname`, so we have to assume the path…
+  const wreiskeIonic4PackagePath = isMeteorProject('.') ?
+    getWReiskeIonic4Package('.') :
+    '.';
 
-	const distPath = wreiskeIonic4PackagePath ?
-		path.join(wreiskeIonic4PackagePath, 'dist') :
-		null;
-	const files = distPath ? getFilesRecursive(distPath) : null;
-	if (files) {
-		const fileRelPathes = files.map(p => {
-			return path.relative(wreiskeIonic4PackagePath, p);
-		});
-		api.addAssets(fileRelPathes, 'client');
-	} else {
-		console.log('Something went wrong: no \'dist\' folder available.');
-	}
+  const distPath = wreiskeIonic4PackagePath ?
+    path.join(wreiskeIonic4PackagePath, 'dist') :
+    null;
+  const files = distPath ? getFilesRecursive(distPath) : null;
+  if (files) {
+    const fileRelPathes = files.map(p => {
+      return path.relative(wreiskeIonic4PackagePath, p);
+    });
+    api.addAssets(fileRelPathes, 'client');
+  } else {
+    console.log('Something went wrong: no \'dist\' folder available.');
+  }
 });
 
 // No test, only side effect of adding Ionic4 assets and loading ionic.js script.
 
 function isMeteorProject(source) {
-	return getChildren(source).indexOf('.meteor') > -1;
+  return getChildren(source).indexOf('.meteor') > -1;
 }
 
 function getWReiskeIonic4Package(source) {
-	const packagesPath = path.resolve(source, 'packages');
-	const packagePathes = getChildren(packagesPath);
-	return packagePathes.find(p => {
-		if (!isDir(p)) {
-			return;
-		}
+  const packagesPath = path.resolve(source, 'packages');
+  const packagePathes = getChildren(packagesPath);
+  return packagePathes.find(p => {
+    if (!isDir(p)) {
+      return;
+    }
 
-		const packageJsonPath = path.resolve(p, 'package.json');
-		const result = isWReiskeIonic4PackageJson(packageJsonPath);
-		return result;
-	});
+    const packageJsonPath = path.resolve(p, 'package.json');
+    const result = isWReiskeIonic4PackageJson(packageJsonPath);
+    return result;
+  });
 }
 
 function isWReiskeIonic4PackageJson(source) {
-	if (!isFile(source)) {
-		return false;
-	}
+  if (!isFile(source)) {
+    return false;
+  }
 
-	const content = fs.readFileSync(source, 'utf8') || '{}'; // Shield against local package with empty package.json.
-	const jsonContent = JSON.parse(content);
-	const packageName = jsonContent.name;
-	return packageName === wreiskeIonic4PackageName;
+  const content = fs.readFileSync(source, 'utf8') || '{}'; // Shield against local package with empty package.json.
+  const jsonContent = JSON.parse(content);
+  const packageName = jsonContent.name;
+  return packageName === wreiskeIonic4PackageName;
 }
 
 function isDir(source) {
-	const stats = fs.lstatSync(source);
+  const stats = fs.lstatSync(source);
 
-	return stats.isSymbolicLink() ? symlinkIsDir(source) : stats.isDirectory();
+  return stats.isSymbolicLink() ? symlinkIsDir(source) : stats.isDirectory();
 }
 
 function isFile(source) {
-	try {
-		const stats = fs.lstatSync(source);
+  try {
+    const stats = fs.lstatSync(source);
 
-		return stats.isSymbolicLink() ? !symlinkIsDir(source) : stats.isFile();
-	} catch (error) {
-		// If file not found, simply return false.
-		// Otherwise, print the error before returning.
-		if (error.code !== 'ENOENT') {
-			console.warn(error);
-		}
-		return false;
-	}
+    return stats.isSymbolicLink() ? !symlinkIsDir(source) : stats.isFile();
+  } catch (error) {
+    // If file not found, simply return false.
+    // Otherwise, print the error before returning.
+    if (error.code !== 'ENOENT') {
+      console.warn(error);
+    }
+    return false;
+  }
 }
 
 function symlinkIsDir(source) {
-	// We have no direct way to know whether the symbolic link points to a file or a folder,
-	// therefore try to read it as a directory…
-	try {
-		fs.readdirSync(source);
-		return true;
-	} catch (error) {
-		return false;
-	}
+  // We have no direct way to know whether the symbolic link points to a file or a folder,
+  // therefore try to read it as a directory…
+  try {
+    fs.readdirSync(source);
+    return true;
+  } catch (error) {
+    return false;
+  }
 }
 
 function getChildren(source) {
-	return fs.readdirSync(source).map(function (name) {
-		return path.join(source, name);
-	});
+  return fs.readdirSync(source).map(function (name) {
+    return path.join(source, name);
+  });
 }
 
 function getFilesRecursive(source) {
-	const children = getChildren(source);
-	let result = [];
+  const children = getChildren(source);
+  let result = [];
 
-	children.forEach(child => {
-		if (isDir(child)) {
-			result = [...result, ...getFilesRecursive(child)];
-		} else if (isFile(child)) {
-			result.push(child);
-		}
-	});
+  children.forEach(child => {
+    if (isDir(child)) {
+      result = [...result, ...getFilesRecursive(child)];
+    } else if (isFile(child)) {
+      result.push(child);
+    }
+  });
 
-	return result;
+  return result;
 }
--- rmdir.js
+++ rmdir.js
@@ -4,20 +4,20 @@ const path = require('path');
 const folderPath = process.env.DIR || 'node_modules/';
 
 if (!fs.existsSync(folderPath) || !fs.lstatSync(folderPath).isDirectory()) {
-    return;
+  return;
 }
 
 function folderDeepDelete(folderPath) {
-    var files = fs.readdirSync(folderPath);
-    files.forEach(function(filename) {
-        var curPath = path.join(folderPath, filename);
-        if (fs.lstatSync(curPath).isDirectory()) {
-            folderDeepDelete(curPath);
-        } else {
-            fs.unlinkSync(curPath);
-        }
-    });
-    fs.rmdirSync(folderPath);
+  var files = fs.readdirSync(folderPath);
+  files.forEach(function (filename) {
+    var curPath = path.join(folderPath, filename);
+    if (fs.lstatSync(curPath).isDirectory()) {
+      folderDeepDelete(curPath);
+    } else {
+      fs.unlinkSync(curPath);
+    }
+  });
+  fs.rmdirSync(folderPath);
 }
 
 folderDeepDelete(folderPath);
