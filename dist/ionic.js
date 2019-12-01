69f4a02adfd22456983f4a4e5634322e05b65590
Will Reiske <wreiske@mieweb.com>
2019-12-01 13:26:44 -0500

    Some cordova fixes

--- dist/ionic.js
+++ dist/ionic.js
@@ -1,12 +1,22 @@
 (function (doc) {
   var scriptElm = doc.createElement('script');
-  scriptElm.setAttribute('type', 'module');
-  scriptElm.src = '/packages/wreiske_ionic4/dist/ionic/ionic.esm.js';
-  doc.head.appendChild(scriptElm);
-
+  if (navigator.platform != 'iPhone') {
+    scriptElm.setAttribute('type', 'module');
+    if (Meteor.isCordova) {
+      scriptElm.src = '/__cordova/packages/wreiske_ionic4/dist/ionic/ionic.esm.js';
+    } else {
+      scriptElm.src = '/packages/wreiske_ionic4/dist/ionic/ionic.esm.js';
+    }
+    doc.head.appendChild(scriptElm);
+  }
   scriptElm = doc.createElement('script');
-  scriptElm.setAttribute('nomodule', '');
-  scriptElm.src = '/packages/wreiske_ionic4/dist/ionic/ionic.js';
+  if (navigator.platform != 'iPhone') {
+    scriptElm.setAttribute('nomodule', '');
+  }
+  if (Meteor.isCordova) {
+    scriptElm.src = '/__cordova/packages/wreiske_ionic4/dist/ionic/ionic.js';
+  } else {
+    scriptElm.src = '/packages/wreiske_ionic4/dist/ionic/ionic.js';
+  }
   doc.head.appendChild(scriptElm);
-
 })(document);
